36e43f36aeeecb907017071ee667bbb3dcd413d7
Jacob Butler <jacob.william.butler@gmail.com>
2016-11-05 16:44:55 -0400

    Updating models. Fixing Doctor.Phone_Number problem

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Patient, Doctor, Rx, Note, Case, Adherence
+
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Rx)
+admin.site.register(Note)
+admin.site.register(Case)
+admin.site.register(Adherence)
--- website/models.py
+++ website/models.py
@@ -3,11 +3,18 @@ from django.db import models
 # Create your models here.
 class Doctor(models.Model):
 	full_name = models.CharField(max_length=200)
+	work_number = models.IntegerField()
+
+	def __str__(self):
+		return self.full_name
 
 class Adherence(models.Model):
 	target_times = models.DateTimeField()
 	taken_times = models.DateTimeField(null=True)
 	taken = models.BooleanField()
+
+	def __str__(self):
+		return "Adherence for " + str(target_times)
 
 class Rx(models.Model):
 	size = models.IntegerField()
@@ -17,18 +24,30 @@ class Rx(models.Model):
 	drug = models.CharField(max_length=100)
 	adherence = models.ForeignKey(Adherence, on_delete=models.CASCADE, null=True)
 
+	def __str__(self):
+		return self.drug
+
 class Note(models.Model):
 	time_entered = models.DateTimeField()
 	description = models.TextField()
+
+	def __str__(self):
+		return str(self.time_entered) + " note."
 
 class Case(models.Model):
 	active = models.BooleanField(default=True)
 	rxs = models.ForeignKey(Rx, on_delete=models.CASCADE)
 	notes = models.ForeignKey(Note, on_delete=models.CASCADE)
 
+	def __str__(self):
+		return "Case"
+
 class Patient(models.Model):
 	first_name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
 	doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 	cases = models.ForeignKey(Case, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.first_name) + " " + str(self.last_name)
 	
